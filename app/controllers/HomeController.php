d005a520102ed390c8d58c9cfea0670ba4730b2d
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-04-02 15:46:27 +0200

    Documentation for 4.3 added and adjusted to the changes that happened in the 4.3 version

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,26 +2,28 @@
 
 use \Michelf\Markdown;
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
 
-	public function handleRequest(){
+    public function handleRequest()
+    {
 
         // TODO create "page not found" page
         $uri = Request::path();
 
         // Default version of the documentation
-        $version = "4.2";
         $page = 'introduction';
-        $versions = array("4.0", "4.1", "4.2");
+        $versions = array("4.0", "4.1", "4.2", "4.3");
+        $version = end($versions);
 
         // If not the root, then split the uri to find the content
         $segment1 = Request::segment(1);
         $segment2 = Request::segment(2);
 
-        if(!empty($segment1)){
+        if (!empty($segment1)) {
             $version = $segment1;
 
-            if(!empty($segment2)){
+            if (!empty($segment2)) {
                 $page = $segment2;
             }
         }
@@ -29,7 +31,7 @@ class HomeController extends BaseController {
         // Show the correct markdown contents
         $page = __DIR__ . '/docs/' . $version . '/' . $page . '.md';
 
-        if(file_exists($page)){
+        if (file_exists($page)) {
 
             $contents = file_get_contents($page);
             $sidebar = file_get_contents(__DIR__ . '/docs/' . $version . '/sidebar.md');
@@ -47,8 +49,8 @@ class HomeController extends BaseController {
                         ->with('sidebar', $sidebar_html)
                         ->with('content', $contents_html);
 
-        }else{
+        } else {
             \App::abort(400, "The page you were looking for could not be found.");
         }
-	}
+    }
 }
